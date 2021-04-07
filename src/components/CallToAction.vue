24d34fa029b8c131c55f7f6d8fd7b8884de53964
masterdisk <costeaeduardnic@gmail.com>
2021-04-07 11:58:23 +0200

    tiles added for cta and background

--- src/components/CallToAction.vue
+++ src/components/CallToAction.vue
@@ -1,9 +1,40 @@
 <!-- USP's re-usable component -->
 <template>
-  <div class="columns">
-    <div class="column is-one-third">element 1</div>
-    <div class="column">element 2</div>
-    <div class="column">element 3</div>
+  <div class="hero-body">
+    <div class="container">
+      <div class="columns">
+        <div class="tile is-parent">
+          <article class="tile is-child box">
+            <p class="title">Middle box</p>
+            <p class="subtitle">With an image</p>
+            <figure class="image is-4by3">
+              <img src="https://bulma.io/images/placeholders/640x480.png">
+            </figure>
+          </article>
+        </div>
+
+        <div class="tile is-parent">
+          <article class="tile is-child box">
+            <p class="title">Middle box</p>
+            <p class="subtitle">With an image</p>
+            <figure class="image is-4by3">
+              <img src="https://bulma.io/images/placeholders/640x480.png">
+            </figure>
+          </article>
+        </div>
+
+
+        <div class="tile is-parent">
+          <article class="tile is-child box">
+            <p class="title">Middle box</p>
+            <p class="subtitle">With an image</p>
+            <figure class="image is-4by3">
+              <img src="https://bulma.io/images/placeholders/640x480.png">
+            </figure>
+          </article>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,6 +44,9 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.hero-body {
+  position: relative;
+  background-image: url('../../public/images/landingspage/cta/cta.png');
+}
 </style>
